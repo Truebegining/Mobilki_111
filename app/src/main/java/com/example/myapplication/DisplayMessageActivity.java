a434d4d9eb2a577651d9d5c0dd904eb1382183f8
Gadzhiev Samir <daniil.barinov.1996@mail.ru>
2024-02-25 23:36:32 +0300

    Initial commit First PT

---
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        super.onCreate(savedInstanceState);
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+}
